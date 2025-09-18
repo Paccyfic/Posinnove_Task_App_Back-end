17d8b035308c54b90ad87c33b543fcbb6914e8da
Pacific Ndahiro <ndahiropaccyfic@gmail.com>
2025-09-18 22:12:42 +0200

    (fix): Adds proper error handling

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/task_app';
+const MONGODB_URI = process.env.MONGODB_URI ;
 
 const connection = mongoose.createConnection(MONGODB_URI).on('error', (error) => {
     console.log('Error connecting to MongoDB', error);
